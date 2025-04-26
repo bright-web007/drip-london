ea1547ac5d9e182ab89e622e0cfdfb8e27cddf1a
Bright Williams <brightwilliams22222@gmail.com>
2025-04-26 02:18:20 +0100

    add some changes to the contact page

--- src/blocks/Block16/Block16.tsx
+++ src/blocks/Block16/Block16.tsx
@@ -9,26 +9,34 @@ const Block16 = () => {
   return (
     <div className="pt-[36px] pr-[24px] pb-[55px] pl-[24px]">
         
-        <div className="flex  p-[24px] flex-col gap-[10px] bg-[#F4F1E1]">
-         <div className=" flex flex-col items-start self-stretch gap-[32px] border-2 border-[rgba(107,107,107,0.48)] p-[40px] ">
+        <div className="h-[624px] flex  p-[24px] flex-col gap-[10px] bg-[#F4F1E1]">
+         <div className="h-[576px]  flex flex-col items-start self-stretch gap-[32px] border-2 border-[rgba(107,107,107,0.48)] p-[40px] ">
 
-              <div className="gap-[12px]">
-                  <p className="font-thankslabs text-[16px] text-[#898155] not-italic font-[254px] leading-[30px] self-stretch">Phone</p>
-                  <p className="font-montserrat text-[14px]  text-[#898155] not-italic font-[254px] pt-[10px]">+449 654 8789</p>
+              <div className="h-[72px] w-[254]  flex flex-col  self-stretch gap-[12px]">
+                  <p className="h-[30px] w-[254] font-thankslabs text-[16px] text-[#898155] not-italic font-[254px] leading-[30px] self-stretch">Phone</p>
+                  <p className="h-[30px] w-[254] font-montserrat text-[14px]  text-[#898155] not-italic font-[254px]">+449 654 8789</p>
               </div>
-              <div className="gap-[12px]">
-                  <p className="font-thankslabs text-[16px] text-[#898155] not-italic font-[254px] leading-[30px] self-stretch">E-mail</p>
-                  <p className="pt-[10px] font-montserrat text-[14px] underline decoration-solid decoration-skip-[none] decoration-thickness-[auto] decoration-underline-offset-[auto] decoration-underline-position-[from-font]  text-[#898155] not-italic font-[254px]">mailto:reservations@drip.london</p>
+
+              <div className="h-[72px] w-[254]  flex flex-col self-stretch gap-[12px]">
+                  <p className="h-[30px] w-[254] font-thankslabs text-[16px] text-[#898155] not-italic font-[254px] leading-[30px] self-stretch">E-mail</p>
+                  <a href="mailto:reservations@drip.london"
+                     className="h-[30px] w-[254] font-montserrat text-[14px] text-[#898155] underline decoration-solid decoration-skip-ink-none decoration-[auto] underline-offset-auto not-italic"
+                   >
+                          reservations@drip.london
+                  </a>
+
               </div>
-              <div className="gap-[12px]">
-                  <p className="font-thankslabs text-[16px] text-[#898155] not-italic font-[254px] leading-[30px] self-stretch">Address</p>
-                  <p className="pt-[10px] font-montserrat text-[14px]  text-[#898155] not-italic font-[254px]">27 Clerkwenwell road, London EC1M 5RN</p>
+
+              <div className="h-[120px] w-[254]  flex flex-col  self-stretch gap-[12px]">
+                  <p className="h-[30px] w-[254] font-thankslabs text-[16px] text-[#898155] not-italic font-[254px] leading-[30px] self-stretch">Address</p>
+                  <p className="h-[60px] w-[254] font-montserrat text-[14px]  text-[#898155] not-italic font-[254px]">27 Clerkwenwell road, London EC1M 5RN</p>
               </div>
-              <div className="gap-[12px]">
-                  <div>
-                      <p className="font-thankslabs text-[16px] text-[#898155] not-italic font-[254px] leading-[30px] self-stretch">Opening Hours</p>
+
+              <div className="h-[162px] w-[254]  flex flex-col  self-stretch gap-[12px]">
+                  <div  className="h-[60px] w-[254] font-thankslabs text-[16px] text-[#898155] not-italic font-[254px] leading-[30px] self-stretch">
+                      <p>Opening Hours</p>
                   </div>
-                  <div className="pt-[10px] font-montserrat text-[14px]  text-[#898155] not-italic font-[254px]">
+                  <div className="h-[120px] w-[254] font-montserrat text-[14px]  text-[#898155] not-italic font-[254px] leading-[21px] self-stretch">
                       <p>Monday-Closed</p>
                       <p>Tuesday-Closed</p>
                       <p>Wednesday to Sunday: 6:30pm-2:00am</p>
@@ -45,19 +53,19 @@ const Block16 = () => {
 
           {/* savor a tasty meal design */}
 
-        <div className="bg-[#898155] p-[24px]">
+        <div className="bg-[#898155] p-[24px] h-[624px] ">
 
-               <div className="bg-[] border-2 flex flex-col gap-[12px] place-items-center  ">
-                      <div>
+               <div className="h-[576px] border-2 flex flex-col gap-[12px]  items-center justify-center ">
+                      <div className="h-[60px] w-[48px]">
                           <Image src={fork} alt="fork and knife"  />
                       </div>
 
                       <div>
-                        <p className="font-thankslabs text-[white] text-[22px] text-center font-[254px] not-italic leading-[30px] self-stretch">Savor a Tasty Meal</p>
+                        <p className="h-[60px] w-[254px] font-thankslabs text-[white] text-[22px] text-center font-[254px] not-italic leading-[30px] self-stretch">Savor a Tasty Meal</p>
                       </div>
 
                       <div className="flex items-center">
-                              <div>
+                              <div className="h-[24px] w-[16px]">
                                 <Image src={group1} alt="group1" /> 
                               </div>
 
@@ -67,12 +75,12 @@ const Block16 = () => {
                                    </div>
                               </div>
 
-                              <div>
+                              <div className="h-[24px] w-[16px]">
                                 <Image src={group2} alt="group2" />
                               </div>
                       </div>
 
-                      <div className="border-none  aspect-[463.5/309] max-w-[463.5px]">
+                      <div className="border-none  aspect-[463.5/309] ">
                              <Image src={dinning} alt='dinning table' className=" "/>
                       </div>
 
