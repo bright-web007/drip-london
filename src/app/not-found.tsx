f41ef8cb36ff5528679c15f43d76d826f6a058dd
Bright Williams <brightwilliams22222@gmail.com>
2025-05-05 10:09:50 +0100

    designed the responsiveness

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,11 +7,11 @@ const page = () => {
     <div className=" bg-[#afa4a4] w-full h-[70vh]  mt-[-72px] flex items-center justify-center ">
 
     <div className="bg-[white] border-4 shadow-lg border-[white] h-[50%] w-[50%] flex flex-col justify-center  place-items-center ">
-      <h1 className="font-thankslabs  text-[70px]  text-beige-500 ">
+      <h1 className="font-thankslabs  text-[70px]  text-beige-500  md:text-[80px]   lg:text-[85px]   xl:text-[95px]">
         404
       </h1>
 
-      <p className="font-thankslabs  text-[10px]  text-beige-500 mb-[10px]">
+      <p className="font-thankslabs  text-[10px]  text-beige-500 mb-[10px]   md:text-[15px]    xl:text-[20px]">
           -NOT FOUND -
       </p>
 
