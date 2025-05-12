da057f535ae5d682e4c30cbda44a6e3582864783
Bright Williams <brightwilliams22222@gmail.com>
2025-05-12 12:55:46 +0100

    update the menu page

--- src/blocks/Block12/Block12.tsx
+++ src/blocks/Block12/Block12.tsx
@@ -3,6 +3,7 @@ import eggs from '@/assets/eggs.svg';
 import { Button } from '@/components/ui/button';
 import right from '@/assets/right-color.svg';
 import left from '@/assets/left-color.png';
+import { Icon } from '@iconify/react';
 import gift from '@/assets/icons-svg/gift.svg';
 
 const Block12 = () => {
@@ -17,14 +18,15 @@ const Block12 = () => {
           className="object-cover h-[450px] w-[45%]"
         />
         <div className="font-thankslabs text-beige-500">
-          <p className="text-base leading-7 font-medium">25% Off on Weekends</p>
-          <h1 className="uppercase text-3xl leading-9 font-semibold my-6">SUPER DEAL</h1>
+          <p className="text-base leading-7 font-medium">EXPERIENCE DRIP LONDON'S</p>
+          <h1 className="uppercase text-2xl leading-9 font-semibold my-6">BOTTOMLESS BRUNCH</h1>
+          <p className="text-base leading-7 font-medium">Every Sunday 5-9PM</p>
           <div className="flex items-center mt-10 z-10">
             <Image src={left} alt="Drip London Logo" width={12} height={16} color="red" />
             <div className="border border-[#89815578] p-[2px] rounded-[2px]">
               <Button className="group  bg-transparent border border-beige-500 hover:bg-beige-500 text-sm cursor-pointer rounded-[2px] px-6 py-5 text-beige-500  flex items-center justify-center transition-all duration-300">
                 <span className="flex items-center space-x-1 font-monserrat">
-                  <span>Book a Table</span>
+                  <span>Book your brunch table</span>
                   <span className="overflow-hidden max-w-0 opacity-0 group-hover:max-w-[1.2rem] group-hover:opacity-100 transition-all duration-300">
                     →
                   </span>
@@ -40,16 +42,23 @@ const Block12 = () => {
         <div className="bg-beige-400 relative flex items-center justify-center w-full h-[215px] overflow-hidden">
           <div className="font-thankslabs text-beige-500 flex flex-col items-center justify-center">
             <Image src={gift} alt="gift" width={50} height={50} className="w-12 h-12" />
-            <p className="text-xs leading-6 font-medium mb-2 mt-4">Our Gift Card</p>
-            <h1 className="uppercase text-base leading-9 font-medium">Are now available</h1>
+            <p className="text-xs leading-6 font-medium ">£55pp</p>
+            <p className="text-xs leading-6 font-medium ">90 Minutes Unlimited</p>
+            <h1 className="uppercase text-base leading-9 font-medium">Cocktail</h1>
           </div>
           <div className="absolute inset-0 border border-beige-500 m-6" />
         </div>
         <div className="bg-beige-400 relative flex items-center justify-center w-full h-[215px] overflow-hidden">
           <div className="font-thankslabs text-beige-500 flex flex-col items-center justify-center">
-            <Image src={gift} alt="gift" width={50} height={50} className="w-12 h-12" />
-            <p className="text-xs leading-6 font-medium mb-2 mt-4">Our Gift Card</p>
-            <h1 className="uppercase text-base leading-9 font-medium">Are now available</h1>
+          <Icon
+            icon="mdi:chef-hat"
+            width={50}
+            height={50}
+            className="w-12 h-12 text-[#7A6E4F]" 
+            />
+            <p className="text-xs leading-6 font-medium ">£45pp</p>
+            <p className="text-xs leading-6 font-medium ">90 Minutes Unlimited</p>
+            <h1 className="uppercase text-base leading-9 font-medium">Prosecco</h1>
           </div>
           <div className="absolute inset-0 border border-beige-500 m-6" />
         </div>
