90b9183351a056da231106f406fe2e1899c75deb
Bright Williams <brightwilliams22222@gmail.com>
2025-05-27 12:56:16 +0100

    update

--- src/blocks/Block19/Block19.tsx
+++ src/blocks/Block19/Block19.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
-import newsbg from '@/assets/background/latest-news-bg.png'
+import newsbg from '@/assets/background/latest-news-bg.jpg'
 
 const Block19 = () => {
   return (
     <div className="relative w-full h-[70vh] overflow-hidden mt-[-72px]">
       <Image src={newsbg} alt="Menu" className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ease-in-out"
       />
-      <div className="absolute inset-0  flex items-center justify-center ">
+      <div className="absolute inset-0  flex flex-col items-center justify-center ">
         <h1 className="font-thankslabs font-bold text-[14px] sm:text-[22px] md:text-[32px] xl:text-[50px] text-white">
-        Latest update from DRIP LONDON
+        PRIVATE DINING
+        </h1>
+        <h1 className="text-white">
+        Designed for unforgettable celebrations.
         </h1>
       </div>
     </div>
