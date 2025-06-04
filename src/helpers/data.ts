256332d7381bda26bc29a87c11271214e6005a05
Bright Williams <brightwilliams22222@gmail.com>
2025-06-04 16:51:30 +0100

    update

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -306,7 +306,7 @@ export const navLinks = [
 
 export const navLinker = [
   { name: 'Reservations', link: '/reservations#reservation' },
-  { name: 'Menus', link: '/menu' },
+  { name: 'Menus', link: '/menu#menu' },
   { name: 'Private Dining', link: '/private-dining#private-dinning' },
   { name: 'The Experience', link: '/the-experience#experience' },
   { name: 'Book An Event', link: '/book-an-event' },
