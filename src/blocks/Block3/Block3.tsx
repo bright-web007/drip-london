0d9846929619abc91e47c7ee7d4f29887e227dc9
Bright Williams <brightwilliams22222@gmail.com>
2025-06-12 00:05:29 +0100

    added some animation to the home page

--- src/blocks/Block3/Block3.tsx
+++ src/blocks/Block3/Block3.tsx
@@ -1,11 +1,23 @@
 'use client';
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { items } from '@/helpers/data';
 import Image from 'next/image';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import type { Button } from '@/helpers/data';
 
 const Block3 = () => {
   const router = useRouter();
+
+  useEffect(() => {
+    AOS.init({
+      duration: 6000, // slow fade-in
+      easing: 'ease-in-out',
+      once: false,
+      offset: 50,
+    });
+  }, []);
 
   const handleButtonClick = (button: Button) => {
     if (button.actionType === 'navigate' && button.path) {
@@ -17,41 +29,48 @@ const Block3 = () => {
     <div className="bg-[#000000]">
       {items.map((item, index) => (
         <div key={index} className="pt-[100px] lg:pt-[120px] xl:pt-[140px]">
-          {/* Title */}
-          <h2 className="text-[18px] text-white font-thankslabs not-italic leading-[35px] font-bold mb-[30px] text-center sm:text-[26px] md:text-[28px] md:mb-[56px] lg:text-[28px] xl:text-[32px]  xl:pb-[56px]">
+          {/* Title with fade-in */}
+          <h2
+            data-aos="custom-fade-in"
+            data-aos-delay="1000"
+            className="text-[18px] text-white font-thankslabs not-italic leading-[35px] font-bold mb-[30px] text-center sm:text-[26px] md:text-[28px] md:mb-[56px] lg:text-[28px] xl:text-[32px]  xl:pb-[56px]"
+          >
             {item.title}
           </h2>
 
-          {/* Image + Content Wrapper with Conditional Flex Direction */}
+          {/* Image + Content Wrapper with fade-in */}
           <div
             className={`xl:flex xl:items-center xl:gap-[40px] ${
               index === 1 ? 'xl:flex-row-reverse' : ''
             }`}
+            data-aos="custom-fade-in"
+            data-aos-delay="1500"
           >
-            {/* Image with inner shadow */}
+            {/* Image */}
             <div className="w-full xl:w-[976px] mb-[18px] md:mb-[56px] xl:mb-0">
               {item.img && (
                 <div
                   className="relative h-[432px] md:h-[693px] xl:h-[693px] w-full rounded overflow-hidden"
-                  style={{ boxShadow: 'inset 0 0 80px rgba(0,0,0,0.6)' }} // Inner shadow
+                  style={{ boxShadow: 'inset 0 0 80px rgba(0,0,0,0.6)' }}
+                  data-aos="custom-fade-in"
+                  data-aos-delay="2000"
                 >
-                  <div className="h-[50px] w-[4334px] border-2 border-[black]  absolute top-[2px] left-1/2 transform -translate-x-1/2 blur-[14.75px] bg-[black]"></div>
+                  <div className="h-[50px] w-[4334px] border-2 border-[black] absolute top-[2px] left-1/2 transform -translate-x-1/2 blur-[14.75px] bg-[black]"></div>
                   <Image src={item.img} alt={item.title} fill className="object-cover" />
-                  {/* Bottom Blur */}
-                  <div className="h-[70px] w-[4334px] border-2 border-[black]  absolute bottom-[-35px] left-1/2 transform -translate-x-1/2 blur-[11.75px] bg-[black]"></div>
-                  {/* Top Blur */}
-                  <div className="hidden h-[50px] w-[4334px] absolute top-[-20px] left-0 blur-[11.75px] transform -translate-x-1/2 bg-black  md:flex"></div>
-                  {/* Left Blur */}
+                  <div className="h-[70px] w-[4334px] border-2 border-[black] absolute bottom-[-35px] left-1/2 transform -translate-x-1/2 blur-[11.75px] bg-[black]"></div>
+                  <div className="hidden h-[50px] w-[4334px] absolute top-[-20px] left-0 blur-[11.75px] transform -translate-x-1/2 bg-black md:flex"></div>
                   <div className="hidden w-[50px] h-full absolute top-0 left-0 blur-[11.75px] bg-black transform -translate-x-1/2 xl:flex"></div>
-
-                  {/* Right Blur */}
                   <div className="hidden w-[50px] h-full absolute top-0 right-[-45px] blur-[11.75px] bg-black transform -translate-x-1/2 xl:flex"></div>
                 </div>
               )}
             </div>
 
-            {/* Content */}
-            <div className="w-full py-[24px] px-[28px] md:px-[40px] xl:w-[800px] xl:px-[82px] xl:flex xl:flex-col xl:justify-center">
+            {/* Text content with fade-in */}
+            <div
+              className="w-full py-[24px] px-[28px] md:px-[40px] xl:w-[800px] xl:px-[82px] xl:flex xl:flex-col xl:justify-center"
+              data-aos="custom-fade-in"
+              data-aos-delay="1000"
+            >
               {item.subtitle && (
                 <h3 className="font-normal font-Montserrat text-[10px] mb-4 sm:mb-6 text-[#898155] sm:text-[16px] md:text-[18px] md:mb-4 xl:text-[16px]">
                   {item.subtitle}
@@ -67,7 +86,9 @@ const Block3 = () => {
                   <button
                     key={btnIndex}
                     onClick={() => handleButtonClick(button)}
-                    className="bg-[rgba(137,129,85,0.15)] font-normal cursor-pointer h-[50px] sm:h-[62px] w-full px-4  py-2 border-[0.5px] border-[#3d3926] rounded-[4px] text-[#cfcfcf] text-[14px] sm:text-[18px] md:text-[18px] md:py-[12px] md:h-[70px]  xl:w-[438px]  xl:h-[50px] xl:text-[16px]"
+                    data-aos="custom-fade-in"
+                    data-aos-delay={`${800 + btnIndex * 300}`}
+                    className="bg-[rgba(137,129,85,0.15)] font-normal cursor-pointer h-[50px] sm:h-[62px] w-full px-4 py-2 border-[0.5px] border-[#3d3926] rounded-[4px] text-[#cfcfcf] text-[14px] sm:text-[18px] md:text-[18px] md:py-[12px] md:h-[70px] xl:w-[438px] xl:h-[50px] xl:text-[16px]"
                   >
                     {button.label}
                   </button>
