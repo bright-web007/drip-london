3c1565a92119c9f7f31a06aa99cf9d12c16d8786
bright <brightwilliams22222@gmail.com>
2026-09-23 13:00:45 +0100

    adjusted the next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,39 @@
+// import type { NextConfig } from 'next';
+// import './env';
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   env: {
+//     // Provide the placeholder for build time
+//     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://placeholder-for-build-time.com',
+//   },
+//   images: {
+//     domains: [process.env.NEXT_PUBLIC_IMAGE_BASE_URL || 'drip-london1.s3.eu-north-1.amazonaws.com'],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from 'next';
 import './env';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   env: {
-    // Provide the placeholder for build time
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://placeholder-for-build-time.com',
   },
   images: {
-    domains: [process.env.NEXT_PUBLIC_IMAGE_BASE_URL || 'drip-london1.s3.eu-north-1.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dolos.hivedeck.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drip-london1.s3.eu-north-1.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
